d4d8854c53955cc5488138370ec7882b80ceeef8
StevenFromSales <46251736+StevenFromSales@users.noreply.github.com>
2018-12-30 04:50:16 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,23 +1,29 @@
 /*
  * Automatic Plant Watering System
  *
- * Created on: 2018-12-29
+ * Created on: 2018-12-30
  * Author: Richardo Prajogo
  *
- * Description : Automatically waters 3 different pot of plants sequentially if they are dry. Uses MSP430G2553 MCU.
+ * Description: Automatically waters 3 different pot of plants sequentially if they are dry. Uses MSP430G2553 MCU.
  *
+ * Changelog
+ * 2018-12-30: Compartmentalized into 4 functions.
  */
 
 
 #include <msp430.h> 
 #include <stdint.h>
 
-#define PLANT1 1
-#define PLANT2 2
-#define PLANT3 3
-
 #define MOISTURE_MIN 200
 #define MOISTURE_MAX 600
+
+struct plantProperty
+{
+    int enableADC;
+    int selectADC;
+    int sampleADC;
+    int activateSolenoid;
+};
 
 /*
  * Disable watchdog
@@ -32,21 +38,24 @@ void disableWatchdog(void)
  */
 void initialize(void)
 {
-    __disable_interrupt();                    // Disable global interrupts
+    __disable_interrupt();                      // Disable global interrupts
 
-    BCSCTL1 = CALBC1_1MHZ;                    // Set range to calibrated 1MHz
-    DCOCTL  = CALDCO_1MHZ;                    // Set DCO step and modulation to calibrated 1MHz
+    BCSCTL1 = CALBC1_1MHZ;                      // Set range to calibrated 1MHz
+    DCOCTL  = CALDCO_1MHZ;                      // Set DCO step and modulation to calibrated 1MHz
 
-    BCSCTL1 |= DIVA_3;                        // ACLK/8
-    BCSCTL3 |= XCAP_3;                        //12.5pF cap- setting for 32768Hz crystal
+    BCSCTL1 |= DIVA_3;                          // ACLK/8
+    BCSCTL3 |= XCAP_3;                          // 12.5pF cap- setting for 32768Hz crystal
 
-    P1DIR |= 0x07;                            // Set P1.0, P1.1, P1.2 as output
-    P1OUT &= ~(BIT0 + BIT1 + BIT2);           // Set all output pins to low
+    P1DIR |= 0x07;                              // Set P1.0, P1.1, P1.2 as output
+    P1OUT &= ~(BIT0 + BIT1 + BIT2);             // Set all output pins to low
 
-    P2DIR |= 0x0F;                            // Set P2.0, P2.1, P2.2, P2.3 as output
-    P2OUT &= ~(BIT0 + BIT1 + BIT2 +BIT3);     // Set all output pins to low
+    P2DIR |= 0x0F;                              // Set P2.0, P2.1, P2.2, P2.3 as output
+    P2OUT &= ~(BIT0 + BIT1 + BIT2 +BIT3);       // Set all output pins to low
 
-    __enable_interrupt();                     //Enable global interrupts
+    ADC10CTL1 |= ADC10DIV_3;
+    ADC10CTL0 = ADC10SHT_1 + ADC10ON + ADC10IE; // Turn on ADC, enable interrupt and sample for 8x ADC10CLKs
+
+    __enable_interrupt();                       // Enable global interrupts
 }
 
 /*
@@ -58,11 +67,11 @@ void delay(int seconds)
 {
     if(seconds<120)
     {
-        TACTL = TASSEL_1 + ID_3 + MC_1;        // ACLK, /8, upmode
-        CCTL0 = CCIE;                          // CCR0 interrupt enabled
-        CCR0 = (512* seconds)-1;               // 512 -> 1 sec, 30720 -> 1 min
+        TACTL = TASSEL_1 + ID_3 + MC_1;          // ACLK, /8, upmode
+        CCTL0 = CCIE;                            // CCR0 interrupt enabled
+        CCR0 = (512* seconds)-1;                 // 512 -> 1 sec, 30720 -> 1 min
 
-        _BIS_SR(LPM3_bits + GIE);              // Enter LPM3 w/ interrupt
+        _BIS_SR(LPM3_bits + GIE);                // Enter LPM3 w/ interrupt
     }
 }
 
@@ -79,120 +88,70 @@ void hdelay(int hours)
 }
 
 /*
- * Check the moisture on plant 1 and water if necessary.
+ * Initialize ADC bits.
  */
-void readyPlantOne()
+void initializeADC(struct plantProperty* ptr_plant)
 {
-    int checkMoisture = 0;
-    P1OUT |= BIT0;
     __disable_interrupt();
-    ADC10CTL1 = INCH_3 + ADC10DIV_3;
-    ADC10CTL0 = ADC10SHT_1 + ADC10ON + ADC10IE;
-    ADC10AE0 |= BIT3;
-    __enable_interrupt();
-    ADC10CTL0 |= ENC + ADC10SC;
-    _BIS_SR(LPM0_bits + GIE);
-    checkMoisture = ADC10MEM;
-    P1OUT &= ~BIT0;
-    if(checkMoisture<MOISTURE_MIN)
-    {
-        P2OUT |= BIT0;
-        delay(1);
-        while(checkMoisture<MOISTURE_MAX)
-        {
-            P2OUT |= BIT3;
-            P1OUT |= BIT0;
-            ADC10CTL0 |= ENC + ADC10SC;
-            _BIS_SR(LPM0_bits + GIE);
-            checkMoisture = ADC10MEM;
-            P1OUT &= ~BIT0;
-        }
-        P2OUT &= ~BIT3;
-        delay(2);
-        P2OUT &= ~BIT0;
-    }
-    __disable_interrupt();
-    ADC10CTL0 &= ~ENC;
-    ADC10AE0 &= ~BIT3;
+    P1OUT |= (*ptr_plant).enableADC;
+    ADC10CTL1 = (*ptr_plant).selectADC;
+    ADC10AE0 |= (*ptr_plant).sampleADC;
     __enable_interrupt();
 }
 
 /*
- * Check the moisture on plant 2 and water if necessary.
+ * De-initialize ADC bits.
  */
-void readyPlantTwo()
+void deinitializeADC(struct plantProperty* ptr_plant)
 {
-    int checkMoisture = 0;
-    P1OUT |= BIT1;
-    __disable_interrupt();
-    ADC10CTL1 = INCH_4 + ADC10DIV_3;
-    ADC10CTL0 = ADC10SHT_1 + ADC10ON + ADC10IE;
-    ADC10AE0 |= BIT4;
-    __enable_interrupt();
-    ADC10CTL0 |= ENC + ADC10SC;
-    _BIS_SR(LPM0_bits + GIE);
-    checkMoisture = ADC10MEM;
-    P1OUT &= ~BIT1;
-    if(checkMoisture<MOISTURE_MIN)
-    {
-        P2OUT |= BIT1;
-        delay(1);
-        while(checkMoisture<MOISTURE_MAX)
-        {
-            P2OUT |= BIT3;
-            P1OUT |= BIT1;
-            ADC10CTL0 |= ENC + ADC10SC;
-            _BIS_SR(LPM0_bits + GIE);
-            checkMoisture = ADC10MEM;
-            P1OUT &= ~BIT1;
-        }
-        P2OUT &= ~BIT3;
-        delay(2);
-        P2OUT &= ~BIT1;
-    }
     __disable_interrupt();
     ADC10CTL0 &= ~ENC;
-    ADC10AE0 &= ~BIT4;
+    ADC10AE0 &= ~(*ptr_plant).sampleADC;
+    P1OUT &= ~(*ptr_plant).enableADC;
     __enable_interrupt();
 }
 
 /*
- * Check the moisture on plant 3 and water if necessary.
+ * Check moisture level.
  */
-void readyPlantThree()
+int checkMoisture()
 {
-    int checkMoisture = 0;
-    P1OUT |= BIT2;
-    __disable_interrupt();
-    ADC10CTL1 = INCH_5 + ADC10DIV_3;
-    ADC10CTL0 = ADC10SHT_1 + ADC10ON + ADC10IE;
-    ADC10AE0 |= BIT5;
-    __enable_interrupt();
+    int moisture = 0;
     ADC10CTL0 |= ENC + ADC10SC;
     _BIS_SR(LPM0_bits + GIE);
-    checkMoisture = ADC10MEM;
-    P1OUT &= ~BIT2;
-    if(checkMoisture<MOISTURE_MIN)
+    moisture = ADC10MEM;
+    return moisture;
+}
+
+/*
+ * Runs moisture level check and waters plant if needed.
+ */
+void waterPlant(struct plantProperty* ptr_plant)
+{
+    int moisture = checkMoisture();
+    if(moisture<MOISTURE_MIN)
     {
-        P2OUT |= BIT2;
+        P2OUT |= (*ptr_plant).activateSolenoid;
         delay(1);
-        while(checkMoisture<MOISTURE_MAX)
+        while(moisture<MOISTURE_MAX)
         {
             P2OUT |= BIT3;
-            P1OUT |= BIT2;
-            ADC10CTL0 |= ENC + ADC10SC;
-            _BIS_SR(LPM0_bits + GIE);
-            checkMoisture = ADC10MEM;
-            P1OUT &= ~BIT2;
+            moisture = checkMoisture();
         }
         P2OUT &= ~BIT3;
         delay(2);
-        P2OUT &= ~BIT2;
+        P2OUT &= ~(*ptr_plant).activateSolenoid;
     }
-    __disable_interrupt();
-    ADC10CTL0 &= ~ENC;
-    ADC10AE0 &= ~BIT5;
-    __enable_interrupt();
+}
+
+/*
+ * Runs the sequence required to water plants.
+ */
+void plantState(struct plantProperty* ptr_plant)
+{
+    initializeADC(ptr_plant);
+    waterPlant(ptr_plant);
+    deinitializeADC(ptr_plant);
 }
 
 void main(void)
@@ -201,11 +160,30 @@ void main(void)
 
     initialize();
 
+    struct plantProperty plant1 = {.enableADC = BIT0,
+                                         .selectADC = INCH_3,
+                                         .sampleADC = BIT3,
+                                         .activateSolenoid = BIT0};
+    struct plantProperty *ptr_plant1 = &plant1;
+
+    struct plantProperty plant2 = {.enableADC = BIT1,
+                                         .selectADC = INCH_4,
+                                         .sampleADC = BIT4,
+                                         .activateSolenoid = BIT1};
+    struct plantProperty *ptr_plant2 = &plant2;
+
+    struct plantProperty plant3 = {.enableADC = BIT2,
+                                         .selectADC = INCH_5,
+                                         .sampleADC = BIT5,
+                                         .activateSolenoid = BIT2};
+    struct plantProperty *ptr_plant3 = &plant3;
+
+
     while(1)
     {
-        readyPlantOne();
-        readyPlantTwo();
-        readyPlantThree();
+        plantState(ptr_plant1);
+        plantState(ptr_plant2);
+        plantState(ptr_plant3);
         hdelay(1);
     }
 }
